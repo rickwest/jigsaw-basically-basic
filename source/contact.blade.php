f168e0d08ffa1745b105c605d95c5770c0dfcd26
rick <rickwestdev@gmail.com>
2018-10-14 14:10:40 +0100

    add email address link to contact page

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -6,6 +6,6 @@ title: Contact
 
 @section('content')
     <p>Want to get in touch? Interested in working with me?<br>
-        Drop me an email and I will be sure to get back to you as soon as possible!
+        Drop me an <a href="mailto:{{ $page->email }}">email</a> and I will be sure to get back to you as soon as possible!
     </p>
 @endsection
