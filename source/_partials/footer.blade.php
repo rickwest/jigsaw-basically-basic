1fe40e639d5339a00c6f212adccfac3eaba40c28
rick <rickwestdev@gmail.com>
2019-04-20 13:39:21 +0100

    Add dynamic year in footer

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer id="footer" class="site-footer">
     <div class="inner">
         <div class="copyright">
-            <p>&copy; {{ $page->siteName }} 2018</p>
+            <p>&copy; {{ $page->siteName }} <script type="text/javascript">document.write(new Date().getFullYear());</script></p>
         </div>
     </div>
 </footer>
